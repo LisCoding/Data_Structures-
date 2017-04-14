08aa5b1a68328774467c3f57b0d0ecc1fc7b1c98
Liseth <cardozoliseth@gmail.com>
2017-04-14 12:41:28 -0700

    complete data structures problems

--- lib/data_structures.rb
+++ lib/data_structures.rb
@@ -2,7 +2,7 @@
 
 # Write a method that returns the range of its argument (an array of integers).
 def range(arr)
-  # your code goes here
+  (arr.min...arr.max).to_a.length
 end
 
 # Write a method that returns a boolean indicating whether an array is in sorted
@@ -10,7 +10,7 @@ end
 # whether its operands are equal, e.g., 2 == 2 => true, ["cat", "dog"] ==
 # ["dog", "cat"] => false
 def in_order?(arr)
-  # your code goes here
+  arr == arr.sort
 end
 
 
@@ -18,12 +18,15 @@ end
 
 # Write a method that returns the number of vowels in its argument
 def num_vowels(str)
-  # your code goes here
+  vowels = "aeiou"
+  count = 0
+  str.downcase.chars {|vowel| count += 1 if vowels.include?(vowel)}
+  count
 end
 
 # Write a method that returns its argument with all its vowels removed.
 def devowel(str)
-  # your code goes here
+  str.downcase.delete("aeiou")
 end
 
 
@@ -33,26 +36,30 @@ end
 # non-negative integer in descending order and as strings, e.g.,
 # descending_digits(4291) #=> ["9", "4", "2", "1"]
 def descending_digits(int)
-  # your code goes here
+  int.to_s.chars.sort.reverse
 end
 
 # Write a method that returns a boolean indicating whether a string has
 # repeating letters. Capital letters count as repeats of lowercase ones, e.g.,
 # repeating_letters?("Aa") => true
 def repeating_letters?(str)
-  # your code goes here
+	letters = str.downcase
+  letters.chars {|letter| return true if letters.count(letter) > 1 }
+  false
 end
 
 # Write a method that converts an array of ten integers into a phone number in
 # the format "(123) 456-7890".
 def to_phone_number(arr)
-  # your code goes here
+  number = arr.join
+  "(#{number[0..2]}) #{number[3..5]}-#{number[6..-1]}"
 end
 
 # Write a method that returns the range of a string of comma-separated integers,
 # e.g., str_range("4,1,8") #=> 7
 def str_range(str)
-  # your code goes here
+  digits = str.scan(/\d+/).sort
+  (digits[0]...digits[-1]).to_a.length
 end
 
 
@@ -63,5 +70,5 @@ end
 # provided. HINT: use the take(num) and drop(num) methods. You won't need much
 # code, but the solution is tricky!
 def my_rotate(arr, offset=1)
-  # your code goes here
+  arr.rotate(offset)
 end
